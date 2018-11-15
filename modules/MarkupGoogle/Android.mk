e851b5e15d5a8f7dda468da2e5697670a5f60172
Rashed Abdel-Tawab <rashed@linux.com>
2018-11-15 08:41:14 -0800

    Set MarkupGoogle to platform certificate

--- modules/MarkupGoogle/Android.mk
+++ modules/MarkupGoogle/Android.mk
@@ -3,5 +3,6 @@ include $(CLEAR_VARS)
 include $(GAPPS_CLEAR_VARS)
 LOCAL_MODULE := MarkupGoogle
 LOCAL_PACKAGE_NAME := com.google.android.markup
+GAPPS_LOCAL_CERTIFICATE := platform
 
 include $(BUILD_GAPPS_PREBUILT_APK)
